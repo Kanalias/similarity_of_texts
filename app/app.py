e558d883db72a96044397e523a71c77ebda58d3b
cyber jq <makssims2014@gmail.com>
2022-06-01 11:23:27 +0300

    тренировка модели теперь использует заранее считанные файлы

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from app.tools.doc_reader import DocReader
 from app.tools.docx_tools import DocExecuter
 from app.tools.simmilatity_docs import SimilarityDocs
 from app.tools.text_preprocessing import TextPreprocessing
-
+from typing import List
 
 class App:
 
@@ -33,22 +33,17 @@ class App:
     def read_json(self, path: str = "data.json"):
         return self.doc_reader.read_json(path)
 
-    def train(self, dir: str, model_name: str):
-        documents = []
-        for doc in self.doc_reader.read_files(dir):
-            texts = self.doc_executer.execute_text(doc, split_cell=False, split_paragraphs=True)
-            documents.extend(self.text_preprocessing.get_filter_documents(texts, is_sentences=False)[0])
-
-            # documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(documents)]
+    def train(self, files: List[dict], model_name: str):
+        documents = [filter_text for file in files for filter_text in file["filter_texts"]]
 
         self.w2v.train(documents)
         self.w2v.save(model_name)
 
     def run(self, is_read_data: bool = False, is_train: bool = False):
         dir = "data\original"
-        model_name = "word2vec.model"
+        model_name = "word2vec1.model"
 
         files = self.read_data(dir, "sentences_data.json") if is_read_data else self.read_json(path="data_new1.json")
-        self.train(dir=dir, model_name=model_name) if is_train else self.w2v.load(model_name)
+        self.train(files=files, model_name=model_name) if is_train else self.w2v.load(model_name)
 
         self.similarity_docs.similarity(source_files=files[1:2], target_files=files[1:2])
