52c941e41ea2d0c4c12e8a9307f4b38f5ba06c54
cyber jq <makssims2014@gmail.com>
2022-06-01 10:56:13 +0300

    поддержка множественного стравнения файлов между собой

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ class App:
         files = []
         for index, doc in enumerate(self.doc_reader.read_files(dir)):
             texts = self.doc_executer.execute_text(doc["file"], split_cell=False, split_paragraphs=True)
-            filter_texts = self.text_preprocessing.get_filter_documents(texts, is_sentences=False)[0]
+            filter_texts = self.text_preprocessing.get_filter_documents(texts, is_sentences=True)[0]
 
             files.append({
                 "index": index,
@@ -48,10 +48,7 @@ class App:
         dir = "data\original"
         model_name = "word2vec.model"
 
-        files = self.read_data(dir, "data_new2.json") if is_read_data else self.read_json(path="data_new1.json")
+        files = self.read_data(dir, "sentences_data.json") if is_read_data else self.read_json(path="data_new1.json")
         self.train(dir=dir, model_name=model_name) if is_train else self.w2v.load(model_name)
 
-        source_file = files[0]
-        target_files = list(filter(lambda x: x["file_name"] != source_file["file_name"], files))
-
-        self.similarity_docs.similarity(source_file, target_files[:4])
+        self.similarity_docs.similarity(source_files=files[1:2], target_files=files[1:2])
